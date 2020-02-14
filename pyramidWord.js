d9516f3f556dee8b63df60b0311dcecde180c605
Kenny Mui <klmui@wisc.edu>
2020-02-13 20:04:05 -0600

    Clean up code

--- pyramidWord.js
+++ pyramidWord.js
@@ -20,11 +20,12 @@ $("input[type='text']").keypress(function (event) {
         } else {
             $(this).val("");
             for (var i = 0; i < word.length; i++) {
-                // Check if the word has any spaces
+                // Check if the word has any spaces or is not a letter
                 if (word[i] === " " || (word[i] === word[i].toUpperCase())) {
                     alert("Please enter a word with no spaces and only letters.");
                     return;
                 } else {
+                    // Track the number of times each character appears
                     if (isNaN(letterCounter[word[i]])) {
                         letterCounter[word[i]] = 1;
                         size++;
@@ -33,20 +34,19 @@ $("input[type='text']").keypress(function (event) {
                     }
                 }
             }
-            console.log(letterCounter);
             // Convert the letterCounter to an array to be sorted
             counts = []
             for (var [key, value] of Object.entries(letterCounter)) {
                 counts.push(value);
             }
-            console.log("Counts " + counts);
-            console.log("Counts sorted " + counts.sort());
+
+            // Add the word to the ul and indicate whether it is a pyramid word or not
             if (counts.length === 1) {
                 $("ul").append("<li><span><i class='fa fa-trash'></i></span> <i class='fa fa-check check-li mr-3'></i> " + word + "</li>");
             } else {
                 for (var i = 1; i < size; i++) {
                     if (counts[i] !== counts[i - 1] + 1) {
-                        $("ul").append("<li><span><i class='fa fa-trash'></i></span> <i class='fa fa-times error-li mr-3 text-danger'></i> " + word + "</li>");
+                        $("ul").append("<li><span><i class='fa fa-trash'></i></span> <i class='fa fa-times error-li mr-3'></i> " + word + "</li>");
                         return;
                     }
                 }
